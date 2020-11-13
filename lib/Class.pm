1f377625748abae12711f01d54b1f6742b49a0ef
brian greenfield <briang@cpan.org>
2020-11-13 09:56:31 +0000

    add more perl stuff

--- lib/Class.pm
+++ lib/Class.pm
@@ -1,17 +1,24 @@
-#!/usr/bin/env perl
-
-#:TAGS:
-
+package Class;
 use 5.030;
 
 use strict;  use warnings;  use autodie qw/:all/;
 use experimental qw(signatures);
 
-package Class; {
-    sub new {
-        my $class = shift;
-        return bless {}, $class
-    }
+=head1 NAME
+
+Class - Doesn't really do anything
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+sub new {
+    my $class = shift;
+    return bless {}, $class
 }
 
 1;
